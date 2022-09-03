2cc609adc22e857af7bb3cbc91760afec98d03a4
Naveen <naveen.kaushik.21@mountblue.tech>
2022-09-03 11:12:05 +0530

    upload

--- src/server/problem1.rb
+++ src/server/problem1.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'json'
+require 'gruff'
 matches = CSV.parse(File.read('/home/naveen/Desktop/ruby-ipl-project/src/data/matches.csv'), headers: true)
 
 def matchesPerYear(matches)
@@ -12,7 +13,22 @@ def matchesPerYear(matches)
           result[match['season']]=1
         end  
     end    
-    puts result
-    # File.open('../output/1_total_match_per_year.json', 'wb') { |file| file.puts JSON.pretty_generate(result) }
+    return result
 end
-matchesPerYear(matches);
+output= Hash.new
+output = matchesPerYear(matches);
+puts (output)
+
+graph = Gruff::Bar.new
+graph.x_axis_label = 'season_wise'
+graph.y_axis_label = 'number_of_matches'
+graph.y_axis_increment = 20
+graph.show_labels_for_bar_values = true
+graph.theme = {marker_color: 'black', background_colors: %w[white gray]}
+graph.title = 'Matches Played Per Year'
+
+output.each do |season, matches_count|
+  graph.data(season, matches_count, '#'.concat(Random.bytes(3).unpack1('H*').to_s))
+end
+
+graph.write('../plots/no_of_matches_per_year.png')
--- src/server/problem2.rb
+++ src/server/problem2.rb
@@ -1,9 +1,11 @@
 require 'csv'
-# require 'json'
+require 'json'
+require 'gruff'
 matches = CSV.parse(File.read('/home/naveen/Desktop/ruby-ipl-project/src/data/matches.csv'), headers: true)
+
 def winsPerTeam(matches)
      obj=Hash.new;
-          for year in matches
+     for year in matches
           if(obj[year['winner']]) 
               if(obj[year['winner']][year['season']])
               obj[year['winner']][year['season']]+=1;
@@ -14,9 +16,7 @@ def winsPerTeam(matches)
          obj[year['winner']]={};
          obj[year['winner']][year['season']]=1;
          end  
-        end
-         puts obj
-        #  File.open('../output/2_match_won_per_team_per_year.json', 'wb') { |file| file.puts JSON.pretty_generate(obj) }
-
+     end
+     puts obj
 end
 winsPerTeam(matches);
